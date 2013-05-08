6468ee9cf8424cd915f2efd990ec1feff4650abd
appdisco <appdisco@appdiscoui-Mac-mini.local>
2013-05-08 14:09:16 +0900

    first commit

---
+++ AdlatteJapanTest/AdlatteJapanTest/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  AdlatteJapanTest
+//
+//  Created by appdisco on 13. 5. 8..
+//  Copyright (c) 2013년 appdisco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ AdlatteJapanTest/AdlatteJapanTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AdlatteJapanTest
+//
+//  Created by appdisco on 13. 5. 8..
+//  Copyright (c) 2013년 appdisco. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
